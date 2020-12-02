5787fc73d735337ed1e1697ce401df81010e228c
Jean-Michel Fayard <jmfayard@gmail.com>
2020-12-02 16:24:53 +0100

    Create AppMenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product} from './model/product';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formation-angular';
+  total = 0;
+  products: Product[] = this.initialProducts();
+
+  initialProducts(): Product[] {
+    const product: Product = {
+      description: 'Men Sweatshirt',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+      price: 39,
+      title: 'Men Sweatshirt'
+    };
+    return [product];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MenuComponent } from './menu/menu.component';
   declarations: [
     AppComponent,
     FirstComponent,
-    MenuComponent
+    MenuComponent,
   ],
   imports: [
     BrowserModule
