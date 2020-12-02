623322fa315d8a81791d7f345138b60e760ad447
Jean-Michel Fayard <jmfayard@gmail.com>
2020-12-02 16:09:39 +0100

    Add index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first-component/first.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
