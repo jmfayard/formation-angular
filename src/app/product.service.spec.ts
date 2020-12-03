bbdf758d449c8094805aa64e4e43bffdf0b1b2fb
Jean-Michel Fayard <jmfayard@gmail.com>
2020-12-03 14:45:56 +0100

    services

---
+++ src/app/product.service.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ProductService } from './product.service';
+import {Product} from './model/product';
+
+describe('ProductService', () => {
+  let service: ProductService;
+  const product = {
+    stock: 2,
+    description: 'Men Sweatshirt',
+    photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+    price: 39,
+    title: 'Men Sweatshirt'
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ProductService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should be available and not the last when stock = 2', function() {
+    product.stock = 2;
+    expect(service.isAvailable(product)).toBe(true);
+    expect(service.isTheLast(product)).toBe(false);
+
+  });
+
+  it('should be available and the last when stock is 1', function() {
+    product.stock = 1;
+    expect(service.isAvailable(product)).toBe(true);
+    expect(service.isTheLast(product)).toBe(true);
+  });
+
+  it('should be the last and not available when stock is 0', function() {
+    product.stock = 0;
+    expect(service.isAvailable(product)).toBe(false);
+    expect(service.isTheLast(product)).toBe(false);
+  });
+});
---
+++ src/app/product.service.ts
@@ -0,0 +1,59 @@
+import {Injectable} from '@angular/core';
+import {Product} from './model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  get products(): Product[] {
+    return this._products;
+  }
+  private _products: Product[] = this.initialProducts();
+
+  isTheLast(product: Product): boolean {
+    return product.stock == 1;
+  }
+
+  isAvailable(product: Product): boolean {
+    return product.stock > 0;
+  }
+
+  decreaseStock(product: Product) {
+    product.stock--;
+  }
+
+  initialProducts() {
+    const one: Product = {
+      stock: 2,
+      description: 'Men Sweatshirt',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf344514006a7fe670e2eb/Mockups/front.png',
+      price: 39,
+      title: 'Men Sweatshirt'
+    };
+    const two = {
+      description: 'BIO T-SHIRT WITH CREWNECK - MEN.',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b2911e4ab33424aec592bd6/Mockups/front.png',
+      price: 19,
+      title: 'Men T-Shirt',
+      stock: 4
+    };
+    const three: Product = {
+      stock: 3,
+      description: 'BIO T-SHIRT WITH CREWNECK - WOMEN.',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5b290d26ab33424aec592bd4/Mockups/front.png',
+      price: 30,
+      title: 'T-Shirt women'
+    };
+    const four: Product = {
+      stock: 7,
+      description: 'C0D1NG_TH3_W0RLD, BIO TOTE BAG.',
+      photo: 'https://s3.eu-central-1.amazonaws.com/balibart-s3/Products/5acf160814006a7fe670e2dd/Mockups/front.png',
+      price: 12.50,
+      title: 'Tote bag'
+    };
+    return [one, two, three, four];
+  }
+
+  constructor() {
+  }
+}
