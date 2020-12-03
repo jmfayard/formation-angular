2abb31700069f8aa7fde210fce871af094774b9e
Jean-Michel Fayard <jmfayard@gmail.com>
2020-12-03 11:03:48 +0100

    Test that the price update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let node: HTMLElement;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       schemas: [
@@ -12,24 +18,30 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+    node = fixture.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'formation-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('formation-angular');
+    expect(component.title).toEqual('formation-angular');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    expect(node.querySelector('h1').textContent).toContain('Welcome to Zenika Ecommerces');
+  });
+
+  it('should update the title when the user clicks on a product', () => {
+    const product = component.products[0];
+    component.updateBasket(product);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Zenika Ecommerces');
+    expect(node.querySelector('header p').innerHTML).toContain(`${product.price}$`)
+
   });
 });
